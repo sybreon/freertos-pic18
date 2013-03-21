3f7f8491d2fe6f05f87dac80ba4289863530a8b5
Shawn Tan <shawn.tan@aeste.my>
2013-03-21 21:01:07 +0800

    Change memory sizes to 18f8722.

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -72,10 +72,10 @@
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configCPU_CLOCK_HZ				( ( unsigned long ) 20000000 )
+#define configCPU_CLOCK_HZ				( ( unsigned long ) 10000000 )
 #define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 4 )
 #define configMINIMAL_STACK_SIZE		( 105 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) 1024 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) 3072 )
 #define configMAX_TASK_NAME_LEN			( 4 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			1
