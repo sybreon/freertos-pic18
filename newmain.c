750a16cbbeed925f5cd30f0cf96734ed5bfaec5e
Shawn Tan <shawn.tan@aeste.my>
2013-04-10 21:53:27 +0800

    Demo program.

--- newmain.c
+++ newmain.c
@@ -15,56 +15,7 @@
 static void vTask1(void *pvParameters) {
     while (1) {
         LATD ^= 0x1;
-        vTaskDelay(111);
-    }
-}
-
-static void vTask2(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x2;
-        vTaskDelay(222);
-    }
-}
-
-static void vTask3(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x4;
-        vTaskDelay(333);
-    }
-}
-
-static void vTask4(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x8;
-        vTaskDelay(444);
-    }
-}
-
-static void vTask5(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x10;
-        vTaskDelay(555);
-    }
-}
-
-static void vTask6(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x20;
-        vTaskDelay(666);
-    }
-}
-
-static void vTask7(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x40;
-        vTaskDelay(777);
-    }
-}
-
-static void vTask8(void *pvParameters) {
-    while (1) {
-        LATD ^= 0x80;
-        vTaskDelay(888);
+        vTaskDelay(1111);
     }
 }
 
@@ -77,13 +28,6 @@ void main(void) {
     LATD = 0;
     /* Create tasks */
     xTaskCreate(vTask1, "T1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask2, "T2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask3, "T3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask4, "T4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask5, "T5", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask6, "T6", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask7, "T7", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask8, "T8", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     /* Start the scheduler.  Will never return here. */
     vTaskStartScheduler();
